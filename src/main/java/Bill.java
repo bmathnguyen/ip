d4b7e8fbf45f835b6c013bd7ffc54cc0c6c29581
Bach Nguyen <xuanbacha1gv@gmail.com>
2025-08-22 14:14:11 +0800

    Level-0: Add greeting and exit

---
+++ src/main/java/Bill.java
@@ -0,0 +1,11 @@
+public class Bill {
+    public static void main(String[] args) {
+        String hello = "____________________________________________________________\n" +
+                " Hello! I'm Bill\n" +
+                " What can I do for you?\n" +
+                "____________________________________________________________\n" +
+                " Bye. Hope to see you again soon!\n" +
+                "____________________________________________________________";
+        System.out.println(hello);
+    }
+}
