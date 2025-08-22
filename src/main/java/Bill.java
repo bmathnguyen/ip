4c1506b2d6dd232b9e8e29c11418e77ae4d76be5
Bach Nguyen <xuanbacha1gv@gmail.com>
2025-08-22 14:21:30 +0800

    Level-1: Add loop and exit condition

--- src/main/java/Bill.java
+++ src/main/java/Bill.java
@@ -1,11 +1,18 @@
+import java.util.Scanner;
+
 public class Bill {
     public static void main(String[] args) {
-        String hello = "____________________________________________________________\n" +
-                " Hello! I'm Bill\n" +
-                " What can I do for you?\n" +
-                "____________________________________________________________\n" +
-                " Bye. Hope to see you again soon!\n" +
-                "____________________________________________________________";
-        System.out.println(hello);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Hello! I'm Bill");
+        System.out.println("What can I do for you?");
+        while (true) {
+            String input = sc.nextLine();
+            if (input.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            }
+            System.out.println(input); // echo
+        }
+        sc.close();
     }
 }
